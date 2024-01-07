3e70042b3deb3ecd179c9e600e57aa7fbaf53141
Nolankicks <95300917+Nolankicks@users.noreply.github.com>
2024-01-07 17:09:18 -0500

    Update trigger.cs

--- trigger.cs
+++ trigger.cs
@@ -1,30 +1,50 @@
+using System.Runtime.CompilerServices;
 using Sandbox;
 
 public sealed class Trigger : Component, Component.ITriggerListener //Change "Trigger to the name of your file
 {
 
  bool _iTouching; 
-  
-    protected override void OnStart()
+ 
+
+
+
+	protected override void OnStart()
     {
       _iTouching = false;
+
     }
 
-    void ITriggerListener.OnTriggerEnter( Collider other ) 
+    void ITriggerListener.OnTriggerEnter(Collider other)
     {
-        _iTouching = true;
+		
+
+		_iTouching = true;
         Log.Info("In");
-       
-        
+       GameManager.ActiveScene.Load(sceneFile);
+      Sound.Play(soundEvent);
+
+	   
+
+         
     }
 
     void ITriggerListener.OnTriggerExit( Collider other ) 
     {
-        _iTouching = false;
-        Log.Info("Out");
-		GameManager.ActiveScene.Load(scene);
 
-        
+		
+	Log.Info("Out");
+	
+    
+
     }
-	[Property] public SceneFile scene { get; set; }
+	[Property] public SceneFile sceneFile {get; set;}
+  [Property] public SoundEvent soundEvent {get; set;}
+	//public Vector3( float x, float y, float z );
+	//[Property] Vector3 vector { get; set; }
+	//[Property] Rotation rotation {get; set;}
+	
+
+
+	
 }
