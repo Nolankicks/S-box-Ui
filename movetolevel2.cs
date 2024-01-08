246741d0bdc42f4f750ca022b8e4422879046f53
Nolankicks <95300917+Nolankicks@users.noreply.github.com>
2024-01-08 11:32:51 -0500

    Create movetolevel2.cs

---
+++ movetolevel2.cs
@@ -0,0 +1,36 @@
+using Sandbox;
+
+public sealed class movetolevel2 : Component, Component.ITriggerListener
+{
+
+void changeScene() //change scene void to load the newScene defined above
+	{
+		Log.Info("aaa");
+		GameManager.ActiveScene.Load(newScene);
+	}	
+[Property] public SceneFile newScene { get; set; }
+ bool _iTouching; 
+  
+    protected override void OnStart()
+    {
+      _iTouching = false;
+    }
+
+    void ITriggerListener.OnTriggerEnter( Collider other ) 
+    {
+        _iTouching = true;
+        Log.Info("In");
+       
+        
+    }
+
+    void ITriggerListener.OnTriggerExit( Collider other ) 
+    {
+        _iTouching = false;
+        Log.Info("Out");
+		GameManager.ActiveScene.Load(newScene);
+
+        
+    }
+
+}
